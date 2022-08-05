cbd627aa432a73e2b452d855ccc5c8602ad8cfd6
Calvin <ncalvin67@gmail.com>
2022-08-05 22:56:15 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const element = document.getElementById("main");
+element.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id', 'victory');
+const node = document.createTextNode("YOUR-NAME is the champion");
+newHeader.appendChild(node);
+const elemnt = document.getElementById("victory");
+element.appendChild(newHeader);
